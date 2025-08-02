ba0f75e3e96ac460b15005a32cc5b9904cdb7e10
Eduardo-1921 <manuel@gmail.com>
2025-08-02 11:04:57 -0500

    subiendo archivos

---
+++ variable.js
@@ -0,0 +1,13 @@
+var numero = 8;
+console.log("el valor del numero:"+numero);
+let nombre = "alejandra";
+var edad = 18;
+let sueldo = 20.000;
+var hijos = 1;
+console.log("su nombre es:"+nombre,"su edad es:"+edad,"su sueldo sera de:"+sueldo,"y usted quiere:"+hijos + "hijo");
+let femenino = true;
+let masculino = false;
+console.log("mi genero es femenino:"+femenino+"mi genero es masculino:"+masculino);
+
+
+
