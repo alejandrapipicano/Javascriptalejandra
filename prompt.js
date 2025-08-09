0de8fd8f77dd9a3b51ae8d1ee0ca03f836ac2896
alejandrapipicano <alemgomz12@gmail.com>
2025-08-09 12:04:13 -0500

    prompt

---
+++ prompt.js
@@ -0,0 +1,7 @@
+let palabra1 = prompt ("digite la palabra1 a concatenar");
+palabra1=parseInt(palabra1);
+let palabra2 =prompt ("deigite la palabra2 a concatenar");
+palabra2=parseInt(palabra2);
+let concatenar = palabra1 + palabra2;
+console.log(concatenar);
+alert("esta es mi palabra concatenada"+concatenar);
