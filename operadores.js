107e4e346b8cc9f76e24197f56ebdcc89668c047
alejandrapipicano <alemgomz12@gmail.com>
2025-08-09 09:24:33 -0500

    operadores

---
+++ operadores.js
@@ -0,0 +1,36 @@
+//suma,resta,multiplicacion y divicion
+//calcular valores de dos o mas elementos
+let num1 = 4;
+var num2 = 8;
+let suma = num1 + num2;
+console.log("el resultado es:" + suma)
+//contadores
+let edad = 18;
+edad=edad+1; //edad++
+console.log("edad es:"+edad);
+//resta
+let nume1 = 6;
+var nume2 = 3;
+let resta = nume1 - nume2;
+console.log("el resultado es:"+resta);
+//contadores
+let hijos = 5;
+hijos = hijos-1; //hijos--
+console.log("hijos es:"+hijos);
+//multiplicacion
+let mult1 =5;
+var mult2 = 6;
+let multi = mult1 * mult2;
+console.log ("el resultado es:" +multi);
+//divicion
+let div1 = 8;
+var div2 =4;
+let divi = div1 / div2;
+console.log("el resultado es:"+divi);
+//residuo
+let mod1 = 6;
+var mod2 = 3;
+let mod = mod1 % mod2;
+console.log ("el resultado es:"+mod);
+
+
