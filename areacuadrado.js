0477ab089bd6a923050cb232ddc07917dc6f95cd
alejandrapipicano <alemgomz12@gmail.com>
2025-08-15 21:28:27 -0500

    areacuadrado

---
+++ areacuadrado.js
@@ -0,0 +1,13 @@
+let lado= prompt ("digite el lado1 del cuadrado");
+lado=parseInt(lado);
+"lado"=="lado"? console.log("el lado igual a lado"):console.log("el lado es diferente a el lado");
+
+let area = lado* lado;
+console.log(area);
+alert("esta es el area del cuadrado"+area)
+
+
+
+
+
+
---
+++ comparacion.js
@@ -0,0 +1,16 @@
+console.log(8>2);
+console.log(2<1);
+console.log(2==1);
+console.log(2=="2");
+console.log(2==="2"); 
+
+
+if(7%2==0){
+    console.log("par");
+}
+
+if(7%2!==0){
+    console.log("impar");
+}
+7%2 ==0? console.log("par"):console.log("impar");
+
